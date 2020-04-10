a92b1fd52dfa0f20e953701339282286ac0c2e15
Wilder Villarraga <hormigah@gmail.com>
2020-04-10 11:44:40 -0500

    add modules and themes

---
+++ themes/custom/motollantas/js/nivelics.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Defines Javascript behaviors for the Nivelics Theme.
+ */
+
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.NivelicsTheme = {
+    attach: function (context) {
+      
+    }
+  };
+  
+})(jQuery, Drupal, drupalSettings);
